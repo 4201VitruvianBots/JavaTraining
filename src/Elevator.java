58268c69aa8fd145118e5b24091f24045243e06d
Jonathan Dao <jonathandao0@gmail.com>
2017-06-11 16:12:31 -0700

    Jonathan Dao
    Initial Commit

---
+++ src/Elevator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Elevator {
+	public static void main(String[] args) {
+		// Do not modify these variables
+		int currentLocation = 0; // You can initialize this to any value you want
+		int destination = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while(true){ // This is so you can input multiple times without restarting the program
+			System.out.println("Enter a destination: ");
+			destination = input.nextInt(); // reads the input and set it as the destination
+			
+			// Begin writing your code here:
+			
+			
+			System.out.println("The elevator moved to floor " + destination);
+			currentLocation = destination;
+		}
+	}
+
+}
