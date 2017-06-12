049734e83ce966435c2e8a22bf8276c31eb55a45
unknown <4201mentor@PLTWTeacher1.davinci.local>
2017-06-11 19:00:52 -0700

    David Stuart: First shot at elevator

--- src/Elevator.java
+++ src/Elevator.java
@@ -5,17 +5,40 @@ public class Elevator {
 		// Do not modify these variables
 		int currentLocation = 0; // You can initialize this to any value you want
 		int destination = 0;
+		int floorMove = 0;
 		Scanner input = new Scanner(System.in);
 		
 		while(true){ // This is so you can input multiple times without restarting the program
 			System.out.println("Enter a destination: ");
 			destination = input.nextInt(); // reads the input and set it as the destination
-			
+						
 			// Begin writing your code here:
 			
-			//test2
-			System.out.println("The elevator moved to floor " + destination);
-			currentLocation = destination;
+			if (destination >5 ) {
+				System.out.println("Floor seleceted too high, Floor 5 selected ");
+				destination = 5;
+			} else if (destination < 0) {
+				System.out.println("Floor seleceted too low, Floor 0 selected ");
+				destination = 0;
+			} 
+			
+			floorMove = destination - currentLocation;
+			System.out.println("The elevator is moving to Floor " + destination);
+			
+			while( floorMove != 0 ) {
+				
+				if (floorMove > 0  ) {
+					floorMove = floorMove - 1;
+					currentLocation = currentLocation +1;
+					System.out.println("The elevator has moved to Floor " + currentLocation);		
+				} else if (floorMove < 0  ) {
+					floorMove = floorMove + 1;
+					currentLocation = currentLocation - 1;
+					System.out.println("The elevator has moved to Floor " + currentLocation);
+				} else {
+					System.out.println("The elevator has arrived at the destination, Floor " + currentLocation);
+							}
+			}		
 		}
 	}
 
