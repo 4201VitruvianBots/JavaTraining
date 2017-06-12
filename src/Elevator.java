73c432a9924bb051a461f456fe25c4bcccfbc8a0
VitruvianBots <vitruvianb@gmail.com>
2017-06-11 19:41:59 -0700

    Aaron Marquez
    Added my elevator code, it only breaks if you input if you a decimal or a super big number.

--- src/Elevator.java
+++ src/Elevator.java
@@ -13,9 +13,42 @@ public class Elevator {
 			
 			// Begin writing your code here:
 			
+				switch(destination){
+					case 1: currentLocation ++;
+					System.out.println("Your Current Destination: 1 ");
+						break;
+					case 2: currentLocation +=2;
+					System.out.println("Your elevator passed floor 1 ");
+					System.out.println("Your Current Destination: 2 ");
+						break;
+					case 3: currentLocation +=3;
+					System.out.println("Your elevator passed floor 1 ");
+					System.out.println("Your elevator passed floor 2 ");
+					System.out.println("Your Current Destination: 3 ");
+						break;
+					case 4: currentLocation +=4;
+					System.out.println("Your elevator passed floor 1 ");
+					System.out.println("Your elevator passed floor 2 ");
+					System.out.println("Your elevator passed floor 3 ");
+					System.out.println("Your Current Destination: 4 ");
+						break;
+					case 5: currentLocation +=5;
+					System.out.println("Your elevator passed floor 1 ");
+					System.out.println("Your elevator passed floor 2 ");
+					System.out.println("Your elevator passed floor 3 ");
+					System.out.println("Your elevator passed floor 4 ");
+					System.out.println("Your Current Destination: 5 ");
+						break;
+					
+				}
+				if(destination > 5 ||destination < 0){
+				System.out.println("Error 404: Floor Not Found! ");
+				}
+				else{
+					System.out.println("The elevator moved to floor " + destination);
+					currentLocation = destination;
+				}
 			
-			System.out.println("The elevator moved to floor " + destination);
-			currentLocation = destination;
 		}
 	}
 
