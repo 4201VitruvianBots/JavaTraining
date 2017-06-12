b1c3dac3e2b7a87c24d2a8381957839d014fb0d5
JLEBLANC2016\jerry <jerryleblanc56@gmail.com>
2017-06-11 18:38:54 -0700

    Jerry LeBlanc Initial

--- src/Elevator.java
+++ src/Elevator.java
@@ -6,17 +6,44 @@ public class Elevator {
 		int currentLocation = 0; // You can initialize this to any value you want
 		int destination = 0;
 		Scanner input = new Scanner(System.in);
-		
+
 		while(true){ // This is so you can input multiple times without restarting the program
 			System.out.println("Enter a destination: ");
-			destination = input.nextInt(); // reads the input and set it as the destination
-			
+			String inString = input.next(); // reads the input and set it as the destination
+			try{ 
+				destination = Integer.valueOf(inString);
+			} 
+			catch( Exception e ) {
+				System.out.println("Invalid floor number.  Enter a number.");
+				System.out.println("The elevator remains on floor " + currentLocation);
+			}
 			// Begin writing your code here:
+
+			if( destination < 0 || destination > 5 ) {
+				System.out.println("Invalid floor number.  Try between 0 and 5");
+				System.out.println("The elevator remains on floor " + currentLocation);
+				destination = currentLocation;
+			} else 
+			while( Math.abs(currentLocation - destination) > 0 ) {
+				if( currentLocation < destination ) {
+					currentLocation = goUp( currentLocation);
+				} else if( currentLocation > destination ){
+					currentLocation = goDown( currentLocation);
+				} else break;
+			}
 			
-			
-			System.out.println("The elevator moved to floor " + destination);
-			currentLocation = destination;
+			System.out.println("The elevator moved to floor " + currentLocation);
+//			currentLocation = destination;
 		}
+//		input.close();
 	}
 
+	private static int goUp(int n) {
+		System.out.println("Elevator moved UP from " + n);
+		return(n+1);
+	}
+	private static int goDown(int n) {
+		System.out.println("Elevator moved DOWN from " + n);
+		return(n-1);
+	}
 }
