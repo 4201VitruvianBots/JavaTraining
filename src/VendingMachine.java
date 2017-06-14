f9d69b08af713e072773d2d089c2d6746880f990
VitruvianBots <vitruvianb@gmail.com>
2017-06-13 20:48:35 -0700

    Stephanie Vending machine progress

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -45,6 +45,25 @@ public class VendingMachine {
 				break;
 			case 2: //item check state (you can have a separate one for each item if you want)
 				
+				try{
+				if(itemInput == item1);
+					(item1 >= 1) ? System.out.println("Item1 has been selected. Please collect your item.") && item1-- : System.out.println("Not enough of item1 in stock. Please select again.") && state = 1
+				} catch(Exception e){
+					System.out.println("Error: item1 not in stock");
+				}
+				try{
+				if(itemInput == item2);
+					(item2 >= 1) ? System.out.println("Item2 has been selected. Please collect your item.") && item2-- : System.out.println("Not enough of item2 in stock. Please select again.") && state = 1
+				} catch(Exception e){
+					System.out.println("Error: item2 not in stock");
+				}
+				try{
+				if(itemInput == item2);
+					(item3 >= 1) ? System.out.println("Item3 has been selected. Please collect your item.") && item3-- : System.out.println("Not enough of item3 in stock. Please select again.") && state = 1
+				} catch(Exception e){
+					System.out.println("Error: item3 not in stock");
+				}				
+				break;
 			case 3: // Admin state
 				
 			}
