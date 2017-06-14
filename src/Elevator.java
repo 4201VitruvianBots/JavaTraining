064cf1c9f12a4cd4b91f0c772ae7d09ea9172b7c
Vitruvian Bots <vitruvianb@gmail.com>
2017-06-13 17:23:26 -0700

    David Uehara elevator # 3

--- src/Elevator.java
+++ src/Elevator.java
@@ -20,33 +20,43 @@ public class Elevator {
 			switch ((int)Button){
 			case 1:
 				destination = 1;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 2:
 				destination = 2;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 3:
 				destination = 3;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 4:
 				destination = 4;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 5:
 				destination = 5;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 6:
 				destination = 6;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 7:
 				destination = 7;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 8:
 				destination = 8;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 9:
 				destination = 9;
+				System.out.println("Your destination is " + destination);
 				break;
 			case 10:
 				destination = 10;
+				System.out.println("Your destination is " + destination);
 				break;
 				
 			default:
