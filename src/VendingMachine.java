f81753b4c87f34af1f90c5c77982e10fbc708676
Jonathan Dao <jonathandao0@gmail.com>
2017-06-13 20:50:44 -0700

    Jonathan Dao
    Initial VendingMachine Code

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -12,18 +12,22 @@ public class VendingMachine {
 		int item2 = 3;
 		int item3 = 0;
 		
+		int itemSelection = -1;
 		
 		while(true){
 			switch(state){
 			case 0: // waiting state;
 				Scanner moneyInput = new Scanner(System.in);
 				//string moneyString = 
-				System.out.println("Input Cash. You need $" + String.format("%.2f", (itemCost - currentMoney)) + " more.");
+				System.out.println("Input Cash. You need $" + String.format("%.2f", (itemCost - currentMoney)) + " more. Type (x) to return change.");
 				String input = moneyInput.next();
 				
-				if(input == "Admin")
+				//input == "Admin"
+				if(input.equals("Admin"))
+					state = 4;
+				else if(input.equals("x"))
 					state = 3;
-				else{
+				else {
 					try{
 						currentMoney += Double.valueOf(input);
 					} catch(Exception e){
@@ -35,18 +39,88 @@ public class VendingMachine {
 					state = 1;
 				break;
 			case 1: //item selection state
-				int itemSelection;
+				itemSelection = -1;
 				Scanner itemInput = new Scanner(System.in);
-				System.out.println("Choose an item");
-				itemSelection = itemInput.nextInt();
+				System.out.println("Choose an item (1, 2, 3) or type (x) to return change.");
+				String item = itemInput.next();
 				
-				state = 2;
+				if(item.equals("x"))
+					state = 3;
+				else{
+					try{
+						int inputSelection = Integer.valueOf(item);
+						if(inputSelection >= 1 || inputSelection <= 3){
+							itemSelection = inputSelection;
+							state = 2;
+						} else 
+							System.out.println("Error: Invalid Item Selection");
+						
+					} catch(Exception e){
+						System.out.println("Error: Invalid input");
+					}
+				}
 							
 				break;
 			case 2: //item check state (you can have a separate one for each item if you want)
-				
-			case 3: // Admin state
-				
+				boolean valid = false;
+				switch(itemSelection){
+				case 1:
+					if(item1 > 0){
+						valid = true;
+						item1--;
+					}
+					break;
+				case 2:
+					if(item2 > 0){
+						valid = true;
+						item2--;
+					}
+					break;
+				case 3:
+					if(item3 > 0){
+						valid = true;
+						item3--;
+					}
+					break;
+				}
+				if(valid){
+					currentMoney -= 2;
+					System.out.println("Dispensed item " + itemSelection + ".");
+					state = 3;
+				}
+				else{
+					System.out.println("Item is out of stock.");
+					state = 1;
+				}
+				break;
+			case 3: // Return change state
+				double change = currentMoney;
+				currentMoney = 0;
+				System.out.println("Returning $" + String.format("%.2f", (change)) + " in change.");
+				state = 0;
+				break;
+			case 4: // Admin state
+				System.out.println("Admin Mode.\nCurrent Stock:\nItem1: " + item1 +"\nItem2: "+ item2 + "\nItem3: "+ item3 + "\nSet the item and its quantity");
+				Scanner adminInput = new Scanner(System.in);
+				try{
+					int adminSelect = adminInput.nextInt();
+					int adminQuantity = adminInput.nextInt();
+					switch(adminSelect){
+					case 1:
+						item1 = adminQuantity;
+						break;
+					case 2:
+						item2 = adminQuantity;
+						break;
+					case 3:
+						item3 = adminQuantity;
+						break;
+					}
+					System.out.println("Set Item " + adminSelect + " to " + adminQuantity);
+				} catch(Exception e){
+					System.out.println("Error: Invalid Input");
+				}
+				break;
 			}
 		}
 		
