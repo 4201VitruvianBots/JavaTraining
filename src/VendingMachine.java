2f2773b57cf08e2c525b93c5751d7ccf39bc61d6
VitruvianBots <vitruvianb@gmail.com>
2017-06-15 20:53:38 -0700

    Aaron Marquez
    I just need to fix the problem when after you select the food item, it doesnt't go the way I wanted to.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class VendingMachine {
+//	private static final Object  = null;
+
 	public static void main(String[] args) {
 		// Do not modify these values
 		int state = 0;
@@ -11,17 +13,18 @@ public class VendingMachine {
 		int Chocolate = 4;
 		int Poptarts = 2;
 		int Chips = 0;
-		int Chocolatewanted;
-		int Poptartswanted;
-		int Chipswanted;
-		int cost;
-		int totalMoney;
-		int chocoInventory;
-		int popInventory;
-		int chipInventory;
-		int amountAdded;
-		int amountAdded2;
-		int amountAdded3;
+		int Chocolatewanted = 1;
+		int Poptartswanted = 1;
+		int Chipswanted = 1;
+		int cost = 0;
+		int totalMoney = 0;
+		int chocoInventory = 0;
+		int popInventory = 0;
+		int chipInventory = 0;
+		int amountAdded = 0;
+		int amountAdded2 = 0;
+		int amountAdded3 = 0;
+		int itemSelection = 0;
 		
 		while(true){
 			switch(state){
@@ -39,9 +42,9 @@ public class VendingMachine {
 				if(input.equals("Admin"))
 					state = 3;
 				else{
-					try{
-						currentMoney += Double.valueOf(input);
-					} catch(Exception e){
+						try{
+							currentMoney += Double.valueOf(input);
+						} catch(Exception e){
 						System.out.println("Error: Invalid Input");
 					}
 				}
@@ -50,39 +53,41 @@ public class VendingMachine {
 					state = 1;
 				break;
 			case 1: //item selection state and this where they select the item
-				int itemSelection;
+				
 				Scanner itemInput = new Scanner(System.in);
 				System.out.println("Choose an item");
 				
 				
-				System.out.println("Chose an item");
+				
 				System.out.println("1 = Chocolate");
 				System.out.println("2 = Poptarts");
 				System.out.println("3 = Chips");
 				
+				state = 2;
 				itemSelection = itemInput.nextInt();
 				
-				state = 2;
+				
 							
 					break;
 			case 2: //item check state (you can have a separate one for each item if you want)
-					
+			
+				switch(itemSelection){
 				case 1: System.out.println("Your chocolate has been dropped");
 					currentMoney = currentMoney - cost;
 					state = 0; //this goes back to the waiting stage
-					System.out.println("$" + current + "returned");
+					System.out.println("$" + currentMoney + "returned");
 					currentMoney=0;
 					
 					System.out.println("Your Poptart has been dropped");
 					currentMoney = currentMoney - cost;
 					state = 0; //this goes back to the waiting stage
-					System.out.println("$" + current + "returned");
+					System.out.println("$" + currentMoney + "returned");
 					currentMoney=0;
 					
 					System.out.println("Your Chips has been dropped");
 					currentMoney = currentMoney - cost;
 					state = 0; //this goes back to the waiting stage
-					System.out.println("$" + current + "returned");
+					System.out.println("$" + currentMoney + "returned");
 					currentMoney=0;
 					
 					break;
@@ -93,22 +98,22 @@ public class VendingMachine {
 				System.out.println("How many chocolates do you want to add to the inventory?");
 				chocoInventory = Chocolate + amountAdded;
 				System.out.println(chocoInventory + "Chocolate");
-				System.out.println(Chocolate "has been added to the inventory!");
+				System.out.println(Chocolate + "has been added to the inventory!");
 				System.out.println("There a total of" + Chocolate + "chocolates" );
 
 				System.out.println("How many poptarts do you want to add to the inventory?");
 				popInventory = Poptarts + amountAdded2;
 				System.out.println(chocoInventory + "Poptarts");
-				System.out.println(Chocolate "has been added to the inventory!");
-				System.out.println("There a total of" + Poptars + "poptarts" );
+				System.out.println(Chocolate + "has been added to the inventory!");
+				System.out.println("There a total of" + Poptarts + "poptarts" );
 				
 				System.out.println("How many chips do you want to add to the inventory?");
 				chipInventory = Chips + amountAdded3;
 				System.out.println(chipInventory + "chips");
-				System.out.println(Chips "has been added to the inventory!");
+				System.out.println(Chips + "has been added to the inventory!");
 				System.out.println("There a total of" + Chips + "chips" );
 				
-				if(input.equals("Wating"))
+//				if(input.equals("Waiting"))
 					state = 0;
 				System.out.println("You're now out of Admin State and now in waiting mode");
 				
@@ -116,6 +121,8 @@ public class VendingMachine {
 				
 			}
 		}
-		
+		 
 	}
 }
+}
+
