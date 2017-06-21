a515f0ff9387b5fd4ffd933cd98284503344cd9f
Julliana Tabuada <Clairetabuada@gmail.com>
2017-06-20 20:45:29 -0700

    Julliana Tabuada
    I forgot what version I'm on, but I may add on to this.... eventually.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -24,18 +24,16 @@ public class VendingMachine {
 				
 				
 				if(input.equals ("Admin"))
-					state = 6;
+					state = 7;
 				
 				else{
 					try{
-						currentMoney += Double.valueOf(input);
+						double money = Double.parseDouble(input);
+						currentMoney += money;
 					} catch(Exception e){
 						System.out.println("Error: Invalid Input");
 					}
 				}
-				
-				if(currentMoney >= itemCost)
-					state = 1;
 				break;
 			
 			case 1: //item selection state
@@ -43,15 +41,29 @@ public class VendingMachine {
 				Scanner itemInput = new Scanner(System.in);
 				System.out.println("Input a number for candy. Skittles = 1, Gummy Bears = 2, and Twix = 3.");
 				itemSelection = itemInput.nextInt();
-				state = 2;
+				switch(itemSelection){
+					case 1: 
+						state = 2;
+						break;
+					case 2: 
+						state = 3;
+						break;
+					case 3: 
+						state = 4;
+						break;
+					default : 
+						state = 5;
+						break;
+					}
+				
 							
 				break;
-			case 2: 
 				
-				if (itemSelection == 1)
+				case 2:
+				
 				System.out.println("Right now there are " + item1 + " packets of Skittles.");
 				
-				System.out.println("With your purchase, there are now " + ((item1-=1)) + " packets left.");
+				System.out.println("With your purchase, there are now " + ((--item1)) + " packets left.");
 				currentMoney = currentMoney - itemCost;
 				currentMoney=0;
 						
@@ -61,12 +73,9 @@ public class VendingMachine {
 				
 			case 3: 
 				
-				if (itemSelection == 2)
 					System.out.println("Right now there are " + item2 + " packets of Gummy Bears.");
 					
-				item2-=1;
-					
-					System.out.println("With your purchase, there are now " + ((item2-=1)) + " packets left.");
+					System.out.println("With your purchase, there are now " + ((--item2)) + " packets left.");
 					currentMoney = currentMoney - itemCost;
 					currentMoney=0;
 				state = 0;
@@ -74,9 +83,8 @@ public class VendingMachine {
 				break;
 				
 			case 4:
-				
-				if (itemSelection == 3)
-					System.out.println("There are currently " + item3 + " bars of Twix left and no more available for purchase.");
+
+					System.out.println("There are currently " + item3 + " bars of Twix left.");
 				currentMoney = currentMoney - itemCost;
 				currentMoney=0;
 				state = 0;
@@ -85,14 +93,14 @@ public class VendingMachine {
 				
 			case 5:	
 				
-				if (itemSelection >= 4) 
+				if (itemSelection >= 4 || itemSelection <= 0) 
 					System.out.println("There isn't any candy with the following inputted number."); 
 				
-				state = 7;
-			
+				state = 1;
+				
 				break;
 				
-			case 6: // Admin state (select an item and what item do you want to add)
+			case 7: // Admin state (select an item and what item do you want to add)
 				
 				
 				System.out.println("You are now in admin. Please select an item."); 
@@ -103,31 +111,29 @@ public class VendingMachine {
 					Scanner quantityInput = new Scanner(System.in);
 					
 				if(itemSelection == 1){
-					item1 = quantityInput.nextInt();
+					item1 += quantityInput.nextInt();
 				System.out.println("This item now has " + item1 + " packets.");
 				
-				state = 0;
+					state = 0;
 				}
 				else if(itemSelection == 2){
-					item2 = quantityInput.nextInt();
-				System.out.println("This item now has " + item2 + " packets.");
+					item2 += quantityInput.nextInt();
+					System.out.println("This item now has " + item2 + " packets.");
 				
-				state = 0;
+					state = 0;
 				}
 				else if(itemSelection == 3){
-					item3 = quantityInput.nextInt();	
-				System.out.println("This item now has " + item3 + " packets.");
-				
-				state = 0;
+					item3 += quantityInput.nextInt();	
+					System.out.println("This item now has " + item3 + " packets.");
+					
+					state = 0;
 				}
-				else if(itemSelection >=4) {
+				else if(itemSelection >=4 || itemSelection<=0) {
 					item3 = quantityInput.nextInt();	
-				System.out.println("Item is invalid.");
+					System.out.println("Item is invalid.");
 				}
 				break;
+				}
 			}
-				
 		}
-		
-	}
 }
