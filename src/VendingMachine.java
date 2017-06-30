3cb28f319e12799b732772ecbda1ffbf47625a3a
Vitruvian Bots <vitruvianb@gmail.com>
2017-06-29 18:45:07 -0700

    Eric Banuelos
    Yay. I finished up the Sprite Pepsi and Code delivery states along with the admin state.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -22,7 +22,7 @@ public class VendingMachine {
 				//string moneyString = 
 				System.out.println("Input Cash. You need $" + String.format("%.2f", (itemCost - currentMoney)) + " more.");
 				input = moneyInput.next();
-				
+				change = 0;
 				if(input.equals("Admin"))
 					state = 3;
 				else{
@@ -38,34 +38,143 @@ public class VendingMachine {
 				break;
 			case 1: //item selection state
 				Scanner itemInput = new Scanner(System.in);
-				System.out.println("Choose an item. Type in Coke, Sprite, or Pepsi");
+				System.out.println("Choose an item. Type in Coke, Sprite, or Pepsi. Type x to return change.");
 				itemSelection = itemInput.next();			
 				state = 2;
 							
 				break;
 			case 2: //item check state (you can have a separate one for each item if you want)
-				/*if (itemSelection.equals("Coke"))
+				if (itemSelection.equals("Coke"))
 				{
-					Coke--;
-					System.out.println("Yay Coke. Your Change is $" + change + ".");
-					currentMoney = 0;
-					change= currentMoney - itemCost; 
+					
+					if (Coke > 0){
+						Coke--;
+						System.out.println("Yay Coke. Your Change is $" + change + ".");
+						change= currentMoney - itemCost;
+						currentMoney = 0;	
+						state = 0;
+					}
+					else {
+						System.out.println("Sorry. "+"No Coke Left.");
+						state = 1;
+						//change = currentMoney; 
+						
+					}
+					
+						
 				}
 				
 				if (itemSelection.equals("Pepsi"))
 				{
-					Pepsi--;
-					System.out.println("Yay Pepsi. Your Change is $" + change + ".");
-					currentMoney = 0;
-					change= ; 
+					if (Pepsi > 0){
+						Pepsi--;
+						System.out.println("Yay Pepsi. Your Change is $" + change + ".");
+						change= currentMoney - itemCost;
+						currentMoney = 0;	
+						state = 0;
+					}
+					else {
+						System.out.println("Sorry. "+"No Pepsi Left.");
+						state = 1;
+						//change = currentMoney; 
+						
+					}
 				}
-				*/
-				state = 0;
+				
+				if (itemSelection.equals("Sprite"))
+				{
+					if (Sprite > 0){
+						Sprite--;
+						System.out.println("Yay Sprite. Your Change is $" + change + ".");
+						change= currentMoney - itemCost;
+						currentMoney = 0;	
+						state = 0;
+					}
+					else {
+						System.out.println("Sorry. "+"No Sprite Left.");
+						state = 1;
+						//change = currentMoney; 
+						
+					}
+				}
+				
+				if (itemSelection.equals("x")){
+					System.out.println("Here is your change $" + currentMoney + ". Come Again.") ; 
+					state = 0; 
+					currentMoney=0;
+				}
+
 				break;
 				
-						case 3: // Admin state
-				System.out.println("A");
-				state = 1;
+				case 3: // Admin state
+						System.out.println("A. Please Select Coke, Pepsi or Sprite.");
+						Scanner adminInput = new Scanner(System.in);
+						itemSelection = adminInput.next();
+						
+						if (itemSelection.equals("Coke"))
+						{
+							System.out.println("Please Enter a Quantity");
+							boolean test = true;
+							while(test)
+							{
+								try
+								{
+									int amount = adminInput.nextInt();
+									test = false;
+									Coke = amount;
+									System.out.println("The amount of Coke is now " + amount );
+								}
+								catch(Exception e)
+								{
+									System.out.println("Please input a number.");
+								}
+							}
+						}
+						
+						if (itemSelection.equals("Pepsi"))
+						{
+							System.out.println("Please Enter a Quantity");
+							boolean test = true;
+							while(test)
+							{
+								try
+								{
+									int amount = adminInput.nextInt();
+									test = false;
+									Pepsi = amount;
+									System.out.println("The amount of Pepsi is now " + amount );
+								}
+								catch(Exception e)
+								{
+									System.out.println("Please input a number.");
+								}
+							}
+						}
+						
+						if (itemSelection.equals("Sprite"))
+						{
+							System.out.println("Please Enter a Quantity");
+							boolean test = true;
+							while(test)
+							{
+								try
+								{
+									int amount = adminInput.nextInt();
+									test = false;
+									Sprite = amount;
+									System.out.println("The amount of Sprite is now " + amount );
+								}
+								catch(Exception e)
+								{
+									System.out.println("Please input a number.");
+								}
+							}
+						}
+						
+						if (itemSelection.equals("z"))
+							state=0;
+						
+						break;
 				
 			}
 		}
